d67fb883b50ac72ff73240cfaec9b44827385c3c
note-9 <heemanshbhawsar2004@gmail.com>
2026-01-03 23:00:22 +0530

    starting with love2d

--- build.sh
+++ build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 set -e
-
-CC=clang
-
-SRC_DIR=src
-EXT_DIR=dep
-
-INCLUDES="-I${EXT_DIR}/glad/include -I${EXT_DIR}/glfw/include -I${EXT_DIR}/stb/ -I${EXT_DIR}/"
-LIBS="-lraylib -lpthread -ldl -lm"
-$CC -std=c11 \
-  main.c\
-  $INCLUDES\
-  $LIBS\
-  -o gungale
-
-echo "Compiling project (C++)..."
+love .
---
+++ main.lua
@@ -0,0 +1,3 @@
+function love.draw()
+  love.graphics.rectangle("fill", 200, 200, 85, 100)
+end
